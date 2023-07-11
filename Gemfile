bd14977f444acdb01c6f6d8b922efae0fd734568
Alini <alini.canedo@gmail.com>
2023-07-11 18:10:28 -0300

    add gem administrate

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem 'administrate', '~> 0.18.0'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :answers
+      resources :answer_models
+      resources :multiples
+      resources :questions
+      resources :question_ratings
+      resources :students
+
+      root to: "answers#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
