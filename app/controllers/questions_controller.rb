741b62379450f6acccaebf477d753c635b88d46c
Alini <alini.canedo@gmail.com>
2023-07-12 13:52:30 -0300

    add flash notice

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,27 @@ class QuestionsController < ApplicationController
     end
 
     def create
-        @answer = Answer.create!(answer_params)
-        redirect_to question_path(@answer.question_id + 1)
+        @answer = Answer.new(answer_params)
+        
+        if @answer.save
+            redirect_to next_question_path
+        else
+            flash[:alert] = @answer.errors.full_messages.join(". ")
+            redirect_back fallback_location: root_path
+        end
     end
-
-    private
+      
+      private
+      
+    def next_question_path
+        next_question = Question.where("id > ?", @answer.question_id).order(:id).first
+        if next_question.present?
+            question_path(next_question)
+        else
+            flash[:notice] = "Todas as questões foram respondidas."
+            questions_path
+        end
+    end
 
     def answer_params
         params.require(:answer).permit(:question_id, :student_id, :student_answer, :unknown, :not_applicable)
